3fc3d64d76a3eb67c300e86f5766ffb199e45004
sirishant <baliga.srishant@gmail.com>
2024-11-06 16:28:48 +0530

    Make somewhat responsive

--- lib/start/login.dart
+++ lib/start/login.dart
@@ -3,59 +3,102 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome!'),
+    var isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+
+    return Theme(
+      data: ThemeData(
+        useMaterial3: true,
       ),
-      body: Center(
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Face Recognition', style: TextStyle(fontSize: 20)),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(200, 0, 200, 0),
-                    child: Placeholder(fallbackHeight: 200, fallbackWidth: 200),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 500,
-              child: VerticalDivider(), 
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Login', style: TextStyle(fontSize: 20)),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(150, 0, 150, 0),
-                    child: TextField(
-                      decoration: InputDecoration(labelText: 'Username'),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome!'),
+        ),
+        body: Center(
+          child: isLandscape
+              ? Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Face Recognition', style: TextStyle(fontSize: 20)),
+                          SizedBox(height: 20),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+                            child: Placeholder(fallbackHeight: 200),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(150, 0, 150, 0),
-                    child: TextField(
-                      decoration: InputDecoration(labelText: 'Password'),
-                      obscureText: true,
+                    SizedBox(
+                      height: 500,
+                      child: VerticalDivider(),
                     ),
-                  ),
-                  SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Sign In'),
-                  ),
-                ],
-              ),
-            ),
-          ],
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Login', style: TextStyle(fontSize: 20)),
+                          SizedBox(height: 20),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(150, 0, 150, 0),
+                            child: TextField(
+                              decoration: InputDecoration(labelText: 'Username'),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(150, 0, 150, 0),
+                            child: TextField(
+                              decoration: InputDecoration(labelText: 'Password'),
+                              obscureText: true,
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text('Sign In'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Face Recognition', style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(250, 0, 250, 0),
+                      child: Placeholder(fallbackHeight: 200, fallbackWidth: 200),
+                    ),
+                    SizedBox(height: 120),
+                    Divider(),
+                    SizedBox(height: 120),
+                    Text('Login', style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(150, 0, 150, 0),
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Username'),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(150, 0, 150, 0),
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Sign In'),
+                    ),
+                  ],
+                ),
         ),
       ),
     );
